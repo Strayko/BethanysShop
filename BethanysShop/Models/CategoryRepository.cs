d5ba52ea4bb7e7509abd9ebf5f6a649aaca0a7d5
Strayko <strayko_lol@windowslive.com>
2020-01-13 11:00:40 +0100

    Creating and Initializing the Database

--- BethanysShop/Models/CategoryRepository.cs
+++ BethanysShop/Models/CategoryRepository.cs
@@ -5,8 +5,15 @@ using System.Threading.Tasks;
 
 namespace BethanysShop.Models
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
-        
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Category> AllCategories => _appDbContext.Categories;
     }
 }
