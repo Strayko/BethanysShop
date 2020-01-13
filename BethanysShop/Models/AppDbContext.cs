330bd4178b3d2b279fa3a2753dec636371081b9c
Strayko <strayko_lol@windowslive.com>
2020-01-13 10:14:17 +0100

    Adding EF Core to the Application

---
+++ BethanysShop/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysShop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
